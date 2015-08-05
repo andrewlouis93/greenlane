f8e8624e0eed1eafc94cdf5c45688280b8f9f58d
nathantoews <nathantoews@gmail.com>
2015-08-05 14:55:54 -0400

    font turnDirect tablet

--- frontend/views/EndpointsView.jsx
+++ frontend/views/EndpointsView.jsx
@@ -224,7 +224,7 @@ var Endpoints = React.createClass({
                 return reactComponent;
               })
             }
-          <button id='submitRoute' onClick={this.validate} className="btn-secondary col s9 offset-s1.5 m6 offset-s3">continue
+          <button id='submitRoute' onClick={this.validate} className="btn-secondary col s9 offset-s1.5 m6 offset-m3">continue
           </button>
         </div>
     );
